33d13dccc95a57c298dce91b31dcb75fec51e60d
David Dappa <daviddappa245@gmail.com>
2022-09-28 13:53:59 -0700

    changed the api endpoint url from http to https

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -8,7 +8,7 @@ const MovieSearch = () => {
 
     const searchMovie = async (input) => {
         try {
-            const response = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=7756b182&s=${input}`);
+            const response = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=7756b182&s=${input}`);
             console.log(response.data);
             setMovies(response.data.Search);
        } catch (e) {
